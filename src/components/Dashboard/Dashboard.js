b575f06d7e17909deba1d68972055449bb609326
AshrafulIslamAlif <alif.ashrafulislam@gmail.com>
2022-04-05 22:13:27 +0600

    readme file

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -12,47 +12,55 @@ const Dashboard = () => {
 
 
         <div className='chart-container'>
-            <LineChart width={500} height={400} data={data}>
-                <Line
-                    type="monotone"
-                    dataKey="sell"
+            <div>
+                <LineChart width={400} height={300} data={data}>
+                    <Line
+                        type="monotone"
+                        dataKey="sell"
 
-                ></Line>
-                <XAxis dataKey="month"></XAxis>
-                <YAxis></YAxis>
-            </LineChart>
-            <AreaChart width={500} height={400} data={data}>
-                <defs>
-                    <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-                        <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-                        <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
-                    </linearGradient>
-                    <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-                        <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
-                        <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
-                    </linearGradient>
-                </defs>
-                <XAxis dataKey="month" />
-                <YAxis />
-                <CartesianGrid strokeDasharray="3 3" />
+                    ></Line>
+                    <XAxis dataKey="month"></XAxis>
+                    <YAxis></YAxis>
+                </LineChart>
+            </div>
+            <div>
+                <AreaChart width={400} height={300} data={data}>
+                    <defs>
+                        <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                            <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                            <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+                        </linearGradient>
+                        <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                            <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+                            <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+                        </linearGradient>
+                    </defs>
+                    <XAxis dataKey="month" />
+                    <YAxis />
+                    <CartesianGrid strokeDasharray="3 3" />
 
-                <Area type="monotone" dataKey="investment" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" stackId="1" />
-                <Area type="monotone" dataKey="revenue" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" stackId="1" />
-            </AreaChart>
+                    <Area type="monotone" dataKey="investment" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" stackId="1" />
+                    <Area type="monotone" dataKey="revenue" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" stackId="1" />
+                </AreaChart>
+            </div>
 
-            <BarChart width={500} height={400} data={data}>
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="month" />
-                <YAxis />
-                <Legend />
-                <Bar dataKey="investment" fill="#8884d8" stackId="1" />
-                <Bar dataKey="revenue" fill="#82ca9d" stackId="1" />
-            </BarChart>
+            <div>
+                <BarChart width={400} height={300} data={data}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="month" />
+                    <YAxis />
+                    <Legend />
+                    <Bar dataKey="investment" fill="#8884d8" stackId="1" />
+                    <Bar dataKey="revenue" fill="#82ca9d" stackId="1" />
+                </BarChart>
+            </div>
 
-            <PieChart width={500} height={400}>
-                <Pie data={data} dataKey="investment" cx="50%" cy="50%" outerRadius={50} fill="#8884d8" />
-                <Pie data={data} dataKey="revenue" cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill="#82ca9d" label />
-            </PieChart>
+            <div>
+                <PieChart width={400} height={300}>
+                    <Pie data={data} dataKey="investment" cx="50%" cy="50%" outerRadius={50} fill="#8884d8" />
+                    <Pie data={data} dataKey="revenue" cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill="#82ca9d" label />
+                </PieChart>
+            </div>
         </div>
 
     );
