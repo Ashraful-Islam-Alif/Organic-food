6ee2da2ae68328a2fdd431fd354faa2aa0a290ed
AshrafulIslamAlif <alif.ashrafulislam@gmail.com>
2022-04-05 18:06:32 +0600

    readme added

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Getting Started with Create React App
+# TopSpeed Car Review
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+(https://friendly-faloodeh-555155.netlify.app/).
 
 ## Available Scripts
 
--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,9 +1,23 @@
 import React from 'react';
-
+import './Blogs.css'
 const Blogs = () => {
     return (
-        <div>
-            <h1>From Blogs</h1>
+        <div className='blogs-describe'>
+            <div className='blogs'>
+                <h1>What is Context API?</h1>
+                <p>Context API is used for avoide prop drilling. Context API is used when the data needs globally. We use Context in theme, preferred language, current authenticated user. It allows components to get data from a context rather than from props. </p>
+            </div>
+            <div className='blogs'>
+                <h1>What is Sementic Tag?</h1>
+                <p>Sementic Tag is the Tag by which elements are describe clearly and makes more readable. Some Semetic Tags are, header, article, section, nav. For using Semantic raking of SEO will increase. Finding code is easier but without sementic need to many code.Easily we can interpret the organization of the sreen reader and assistive devices.</p>
+            </div>
+            <div className='blogs'>
+                <h1>Difference between Inline, Inline-block, block.</h1>
+                <p><h6>Inline</h6> It cannot set the width and height. The elements doesnot starts in new line only occupy with width it required</p>
+                <p><h6>Inline-Block</h6>It is almost similar feature line Inline, the only difference is it can set the width and height.</p>
+                <p><h6>Block</h6> It can set the width and height. The elements starts in new line only and occupy with full width it required</p>
+            </div>
+
         </div>
     );
 };
